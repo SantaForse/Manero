4bc9ed5d8166313a4c7c6f23bede129b63cce127
p <p@DESKTOP-UBUDTBS>
2023-10-22 13:25:13 +0200

    Finished design, some stuff left for ease of implementation of multiple login alternatives

---
+++ Manero/Areas/Identity/Pages/Account/PhoneVerifyConfirmation.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Manero.Areas.Identity.Pages.Account
+{
+    public class PhoneVerifyConfirmationModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public PhoneVerifyConfirmationModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            [Required(ErrorMessage = "OTP is required.")]
+            [StringLength(5, ErrorMessage = "The OTP must be 5 characters long.")]
+            public string OTPCode { get; set; }
+        }
+
+        public IActionResult OnGet()
+        {
+            Input = new InputModel { };
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+               var otpCode = Input.OTPCode;
+            }
+
+            return Page();
+        }
+
+    }
+}
