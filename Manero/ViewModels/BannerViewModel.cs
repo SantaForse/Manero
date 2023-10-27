4c7bec79e9d8a0a6efaed9ea58f699a52315e32c
LeniNaple <jesperkun@gmail.com>
2023-10-27 13:54:24 +0200

    Added/changed a few partials and created some viewmodels...

---
+++ Manero/ViewModels/BannerViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Manero.ViewModels;
+
+public class BannerViewModel
+{
+
+    public string Title { get; set; } = null!;
+
+    public string ButtonText { get; set; } = null!;
+
+    public string ButtonLink { get; set; } = null!;
+
+    public string FlexSide { get; set; } = null!;
+
+}
---
+++ Manero/ViewModels/CardGridViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Manero.ViewModels;
+
+public class CardGridViewModel
+{
+    public string Title { get; set; } = null!;
+
+    public string ButtonText { get; set; } = null!;
+
+    public string ButtonLink { get; set; } = null!;
+    
+    public int CardNr { get; set; }
+
+}
