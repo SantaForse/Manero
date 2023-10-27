54c90ae8ad02f0460b9f2eb60bd1ae3d19956223
Anton Lindström <anton.jumkil@gmail.com>
2023-10-27 13:52:33 +0200

    added views etc

---
+++ Manero/Controllers/CheckoutController.cs
@@ -0,0 +1,20 @@
+﻿using Manero.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CheckoutViewModel viewModel)
+        {
+            //return order sucessful view if sucessful, otherwise return order failed view.
+            return View();
+        }
+    }
+}
