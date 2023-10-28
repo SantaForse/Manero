9dd5d1e5660816a50b45aed7d8181aba0f851b08
p <p@DESKTOP-UBUDTBS>
2023-10-28 14:04:17 +0200

    readd DI

--- Manero/Program.cs
+++ Manero/Program.cs
@@ -1,6 +1,7 @@
 using manero.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
