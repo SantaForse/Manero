8a3be4fb58655df8ba986a34ecb41f386dd26041
LeniNaple <jesperkun@gmail.com>
2023-10-29 12:52:11 +0100

    Started a bit of card routing/navigation

---
+++ Manero/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public IActionResult Product()
+    {
+
+        //Will load up product via Id then create a product details page for it... 
+        return View();
+    }
+
+}
