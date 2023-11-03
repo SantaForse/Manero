0506a640d602399b16a9a1aee0f4ed17c8a1af0f
Christian Eriksson <e.christian@live.se>
2023-11-03 17:43:11 +0100

    added superficial viewmodel support for UserProfile, UserAddress & OrderHistory

---
+++ Manero/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manero.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string? FirstName { get; set; } = null!;
+        public string? LastName { get; set; } = null!;
+        public string? Email { get; set; } = null;
+        public string? ImageUrl { get; set; } = null;
+
+
+
+        [Display(Name = "Product Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile? Image { get; set; }
+
+
+
+
+        public string? AddressCategory { get; set; }
+        public string? StreetNumber { get; set; }
+        public string? StreetName { get; set; }
+        public string? City { get; set; }
+
+    }
+}
