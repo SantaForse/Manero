f2ee7645b34b15d7bfdb96b8c225c23628f7079b
LeniNaple <jesperkun@gmail.com>
2023-11-10 15:04:24 +0100

    Lite nya produkter vi har lagt till tillsammans :D

--- Manero/Data/ProductDbContext.cs
+++ Manero/Data/ProductDbContext.cs
@@ -39,18 +39,95 @@ public class ProductDbContext : DbContext
                     Rating = 2,
                     SalePrice = 500,
                     Price = 699,
-                    ImageUrl = "https://media.discordapp.net/attachments/1161545688103006245/1171425219949371442/tactical-beard-brown.png?ex=655ca1c0&is=654a2cc0&hm=44c37be70087bceccb4e1e8b29418f640271e6e32984398ca58d349ba8aeb131&=&width=337&height=368"
+                    ImageUrl = "https://images.pexels.com/photos/291759/pexels-photo-291759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Grön Hoodie",
+                    Id = 3,
+                    //CategoryId = 1, 
+                    Description = "Handgjort vit hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 4,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/981091/pexels-photo-981091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                //New products
+                new ProductEntity
+                {
+                    ProductName = "Brun Hoodie",
+                    Id = 4,
+                    //CategoryId = 1, 
+                    Description = "Handgjort brun hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 5,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/704857/pexels-photo-704857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 },
                 new ProductEntity
                 {
                     ProductName = "Svart Hoodie",
-                    Id = 3,
+                    Id = 5,
                     //CategoryId = 1, 
                     Description = "Handgjort svart hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 5,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/744480/pexels-photo-744480.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Grön klänning",
+                    Id = 6,
+                    //CategoryId = 1, 
+                    Description = "Handgjort Grön klänning är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
                     Rating = 4,
                     SalePrice = 200,
                     Price = 299,
-                    ImageUrl = "https://media.discordapp.net/attachments/1161545688103006245/1171425219949371442/tactical-beard-brown.png?ex=655ca1c0&is=654a2cc0&hm=44c37be70087bceccb4e1e8b29418f640271e6e32984398ca58d349ba8aeb131&=&width=337&height=368"
+                    ImageUrl = "https://images.pexels.com/photos/985635/pexels-photo-985635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Svarta skor",
+                    Id = 7,
+                    //CategoryId = 1, 
+                    Description = "Handgjort svarta skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 3,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/5214139/pexels-photo-5214139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Vita skor",
+                    Id = 8,
+                    //CategoryId = 1, 
+                    Description = "Handgjort vita skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 2,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/5730956/pexels-photo-5730956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Gula skor",
+                    Id = 9,
+                    //CategoryId = 1, 
+                    Description = "Handgjort Gula skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 5,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new ProductEntity
+                {
+                    ProductName = "Bruna skor",
+                    Id = 10,
+                    //CategoryId = 1, 
+                    Description = "Handgjort Bruna skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.",
+                    Rating = 3,
+                    SalePrice = 200,
+                    Price = 299,
+                    ImageUrl = "https://images.pexels.com/photos/2562992/pexels-photo-2562992.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 });
 
         modelBuilder.Entity<CategoryEntity>().HasData(
@@ -78,6 +155,11 @@ public class ProductDbContext : DbContext
                 {
                     Id = 5,
                     CategoryName = "Dresses",
+                },
+                new CategoryEntity
+                {
+                    Id = 6,
+                    CategoryName = "Hoodie",
                 });
 
         modelBuilder.Entity<ProductCategoryEntity>().HasData(
@@ -93,8 +175,63 @@ public class ProductDbContext : DbContext
                 },
                 new ProductCategoryEntity
                 {
-                    ProductId = 1,
+                    ProductId = 3,
+                    CategoryId = 2,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 6,
+                    CategoryId = 5,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 7,
                     CategoryId = 3,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 8,
+                    CategoryId = 3,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 9,
+                    CategoryId = 3,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 10,
+                    CategoryId = 3,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 8,
+                    CategoryId = 2,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 9,
+                    CategoryId = 2,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 10,
+                    CategoryId = 2,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 5,
+                    CategoryId = 6,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 4,
+                    CategoryId = 6,
+                },
+                new ProductCategoryEntity
+                {
+                    ProductId = 3,
+                    CategoryId = 6,
                 });
             }
                 
--- Manero/Migrations/20231110140129_init.cs
+++ Manero/Migrations/20231110140129_init.cs
@@ -76,7 +76,8 @@ namespace Manero.Migrations
                     { 2, "Accessories" },
                     { 3, "Shoes" },
                     { 4, "T-shirts" },
-                    { 5, "Dresses" }
+                    { 5, "Dresses" },
+                    { 6, "Hoodie" }
                 });
 
             migrationBuilder.InsertData(
@@ -85,8 +86,15 @@ namespace Manero.Migrations
                 values: new object[,]
                 {
                     { 1, "Handgjort ansiktsskägg är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://media.discordapp.net/attachments/1161545688103006245/1171425219949371442/tactical-beard-brown.png?ex=655ca1c0&is=654a2cc0&hm=44c37be70087bceccb4e1e8b29418f640271e6e32984398ca58d349ba8aeb131&=&width=337&height=368", 399, "Skägg deluxe", 3, 300 },
-                    { 2, "Handgjort vit klänning är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://media.discordapp.net/attachments/1161545688103006245/1171425219949371442/tactical-beard-brown.png?ex=655ca1c0&is=654a2cc0&hm=44c37be70087bceccb4e1e8b29418f640271e6e32984398ca58d349ba8aeb131&=&width=337&height=368", 699, "Vit klänning", 2, 500 },
-                    { 3, "Handgjort svart hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://media.discordapp.net/attachments/1161545688103006245/1171425219949371442/tactical-beard-brown.png?ex=655ca1c0&is=654a2cc0&hm=44c37be70087bceccb4e1e8b29418f640271e6e32984398ca58d349ba8aeb131&=&width=337&height=368", 299, "Svart Hoodie", 4, 200 }
+                    { 2, "Handgjort vit klänning är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/291759/pexels-photo-291759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 699, "Vit klänning", 2, 500 },
+                    { 3, "Handgjort vit hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/981091/pexels-photo-981091.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Grön Hoodie", 4, 200 },
+                    { 4, "Handgjort brun hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/704857/pexels-photo-704857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Brun Hoodie", 5, 200 },
+                    { 5, "Handgjort svart hoodie är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/744480/pexels-photo-744480.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Svart Hoodie", 5, null },
+                    { 6, "Handgjort Grön klänning är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/985635/pexels-photo-985635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Grön klänning", 4, 200 },
+                    { 7, "Handgjort svarta skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/5214139/pexels-photo-5214139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Svarta skor", 3, 200 },
+                    { 8, "Handgjort vita skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/5730956/pexels-photo-5730956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Vita skor", 2, 200 },
+                    { 9, "Handgjort Gula skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Gula skor", 5, 200 },
+                    { 10, "Handgjort Bruna skor är lämplig för utomhusaktiviteter, överlevnad, alla typer jakt och fiske, så rolig och fantastisk. Dra åt snabbt, lossa eller ta bort skägget med knappfästen, så att ditt ansikte alltid blir varmt och skyddat.", "https://images.pexels.com/photos/2562992/pexels-photo-2562992.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 299, "Bruna skor", 3, 200 }
                 });
 
             migrationBuilder.InsertData(
@@ -95,8 +103,19 @@ namespace Manero.Migrations
                 values: new object[,]
                 {
                     { 2, 1 },
-                    { 3, 1 },
-                    { 5, 2 }
+                    { 5, 2 },
+                    { 2, 3 },
+                    { 6, 3 },
+                    { 6, 4 },
+                    { 6, 5 },
+                    { 5, 6 },
+                    { 3, 7 },
+                    { 2, 8 },
+                    { 3, 8 },
+                    { 2, 9 },
+                    { 3, 9 },
+                    { 2, 10 },
+                    { 3, 10 }
                 });
 
             migrationBuilder.CreateIndex(
