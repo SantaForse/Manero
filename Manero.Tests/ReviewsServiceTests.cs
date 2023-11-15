646809d6eca08c82fc874ce28f9810c3c081720a
zarapar64 <zarapar64@gmail.com>
2023-11-15 20:40:14 +0100

    Fixed rviews test

--- Manero.Tests/ReviewsServiceTests.cs
+++ Manero.Tests/ReviewsServiceTests.cs
@@ -2,127 +2,129 @@
 using Manero.Models.Entities;
 using Manero.Services;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
-namespace Manero.Tests;
-
-// These are Zahra's tests, more tests will be added for reviews part
-public class ProductServiceTests
+namespace Manero.Tests
 {
-
-
-    [Fact]
-    public void ToGet_Reviews_From_Database()
+    // These are Zahra's tests, more tests will be added for reviews part
+    public class ProductServiceTests
     {
-        // Arrange
-        var options = new DbContextOptionsBuilder<ProductDbContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
+        [Fact]
+        public void ToGet_Reviews_From_Database()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ProductDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
 
-        using (var context = new ProductDbContext(options))
+            using (var _context = new ProductDbContext(options))
+            {
+                var reviews = new List<ReviewEntity>
+                {
+                    new ReviewEntity
+                    {
+                        Rating = 5,
+                        CommentText = "Excellent",
+                        ReviewDate = DateTime.Now
+                    },
+                    new ReviewEntity
+                    {
+                        Rating = 3,
+                        CommentText = "Good",
+                        ReviewDate = DateTime.Now
+                    },
+                    new ReviewEntity
+                    {
+                        Rating = 1,
+                        CommentText = "Bad",
+                        ReviewDate = DateTime.Now
+                    }
+                };
+
+                _context.Reviews.AddRange(reviews);
+                _context.SaveChanges();
+            }
+
+            using (var _context = new ProductDbContext(options))
+            {
+                // Act
+                var reviewsFromDb = _context.Reviews.ToList();
+
+                // Assert
+                Assert.NotNull(reviewsFromDb);
+                Assert.Equal(3, reviewsFromDb.Count);
+            }
+        }
+
+        [Fact]
+        public void ToGet_ReviewsFor_SpecificProduct()
         {
 
-            var reviews = new List<ReviewEntity>
+            var options = new DbContextOptionsBuilder<ProductDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using (var _context = new ProductDbContext(options))
             {
-                new ReviewEntity
+                var product = new ProductEntity
+                {
+                    ProductName = "MyTestProduct",
+                    Description = "This product is very nice but this is for test now.",
+                    Rating = 5,
+                    Price = 70,
+                    SalePrice = 40,
+                    ImageUrl = "https://testForproduct.com/image.jpg",
+                };
+
+                var review1 = new ReviewEntity
                 {
                     Rating = 5,
-                    CommentText = "Excellent",
+                    CommentText = "Nice",
                     ReviewDate = DateTime.Now
-                },
-                new ReviewEntity
+                };
+
+                var review2 = new ReviewEntity
                 {
-                    Rating = 3,
+                    Rating = 4,
                     CommentText = "Good",
                     ReviewDate = DateTime.Now
-                },
-                new ReviewEntity
-                {
-                    Rating = 1,
-                    CommentText = "Bad",
-                    ReviewDate = DateTime.Now
-                }
-            };
+                };
 
-            context.Reviews.AddRange(reviews);
-            context.SaveChanges();
+                _context.Products.Add(product);
+                _context.Reviews.AddRange(review1, review2);
+
+                var productReview1 = new ProductReviewEntity { ProductId = product.Id, ReviewId = review1.Id };
+                var productReview2 = new ProductReviewEntity { ProductId = product.Id, ReviewId = review2.Id };
+
+                _context.ProductReviews.AddRange(productReview1, productReview2);
+
+                _context.SaveChanges();
+            }
+
+            using (var _context = new ProductDbContext(options))
+            {
+                var productService = new ProductsService(_context);
+                var reviewService = new ReviewService(_context, productService);
+
+
+                var product = _context.Products.SingleOrDefault(p => p.ProductName == "MyTestProduct");
+
+
+                Assert.NotNull(product);
+                var reviews = reviewService.GetReviews(product.Id);
+
+
+                Assert.NotNull(reviews);
+                Assert.Equal(2, reviews.Count());
+            }
         }
 
-        using (var context = new ProductDbContext(options))
-        {
-            // Act
-            var reviewsFromDb = context.Reviews.ToList();
-
-            // Assert
-            Assert.NotNull(reviewsFromDb);
-            Assert.Equal(3, reviewsFromDb.Count);
-        }
     }
-
-
-
-
-
-
-    [Fact]
-    public void ToGet_ReviewsFor_SpecificProduct()
-    {
-        // Arrange
-        var options = new DbContextOptionsBuilder<ProductDbContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
-
-        using (var context = new ProductDbContext(options))
-        {
-            var product = new ProductEntity
-            {
-                ProductName = "MyTestProduct",
-                Description = "This product is very nice but this is for test now.",
-                Rating = 5,
-                Price = 70,
-                SalePrice = 40,
-                ImageUrl = "https://testForproduct.com/image.jpg",
-
-            };
-
-            var review1 = new ReviewEntity
-            {
-                Rating = 5,
-                CommentText = "Nice",
-                ReviewDate = DateTime.Now
-            };
-
-            var review2 = new ReviewEntity
-            {
-                Rating = 4,
-                CommentText = "Good",
-                ReviewDate = DateTime.Now
-            };
-
-            context.Products.Add(product);
-            context.Reviews.AddRange(review1, review2);
-
-            var productReview1 = new ProductReviewEntity { ProductId = product.Id, ReviewId = review1.Id };
-            var productReview2 = new ProductReviewEntity { ProductId = product.Id, ReviewId = review2.Id };
-
-            context.ProductReviews.AddRange(productReview1, productReview2);
-
-            context.SaveChanges();
-        }
-
-        using (var context = new ProductDbContext(options))
-        {
-            var productService = new ProductsService(context);
-            var reviewService = new ReviewService(context, productService);
-
-
-            var product = context.Products.Single(p => p.ProductName == "TestProduct");
-            var reviews = reviewService.GetReviews(product.Id);
-
-            // Assert
-            Assert.NotNull(reviews);
-            Assert.Equal(2, reviews.Count());
-        }
-    }
-
-
 }
+
+
+
+
